8ccbf931d2960974f5657aeaa9e426dbb1a4cfe3
hajSaaa <sassi_hajer@yahoo.fr>
2023-11-05 15:14:08 +0100

    authentification

---
+++ src/test/java/com/e2eTest/automation/step_definitions/AuthentificationStepDefinition.java
@@ -0,0 +1,34 @@
+package com.e2eTest.automation.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AuthentificationStepDefinition {
+
+	@Given("Je me connecte sur l application nopeCommerce")
+	public void jeMeConnecteSurLApplicationNopeCommerce() {
+
+	}
+
+	@When("Je saisis l email {string}")
+	public void jeSaisisLEmail(String string) {
+
+	}
+
+	@When("Je saisis le password {string}")
+	public void jeSaisisLePassword(String string) {
+
+	}
+
+	@When("Je clique sur le bouton LOG IN")
+	public void jeCliqueSurLeBoutonLOGIN() {
+
+	}
+
+	@Then("Je me redirige vers la page home")
+	public void jeMeRedirigeVersLaPageHome() {
+
+	}
+
+}
